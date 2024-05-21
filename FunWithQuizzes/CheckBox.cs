844363e371bbf4c6caef9d7cb2acb4fd7339f808
Your Name <108307350+pjduri@users.noreply.github.com>
2024-05-21 15:07:30 -0500

    checkbox

---
+++ FunWithQuizzes/CheckBox.cs
@@ -0,0 +1,25 @@
+﻿namespace FunWithQuizzes;
+
+public class CheckBox : Question
+{
+
+    public List<string> PossibleAnswers { get; set; }
+
+    public CheckBox(string questionText, int pointValue = PossibleAnswers.Count, List<string> possibleAnswers, List<string> correctAnswers) : base(questionText, pointValue, correctAnswers)
+    {
+        PossibleAnswers = possibleAnswers;
+    }
+
+    public override int ScoreQuestion()
+    {
+        int numCorrect = 0;
+        for (int i=0; i<possibleAnswers.Count; i++)
+        {
+            if (possibleAnswers[i] == CorrectAnswers[i])
+            {
+                numCorrect++;
+            }
+        }
+        return numCorrect / PointValue;
+    }
+}
--- FunWithQuizzes/obj/Debug/net8.0/FunWithQuizzes.AssemblyInfo.cs
+++ FunWithQuizzes/obj/Debug/net8.0/FunWithQuizzes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FunWithQuizzes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7880bfec4ac7af4de71037b7523e33b0ed69003a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4c6559a0f5f94813b7f8e3f5df9fdd06561f9a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FunWithQuizzes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FunWithQuizzes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
