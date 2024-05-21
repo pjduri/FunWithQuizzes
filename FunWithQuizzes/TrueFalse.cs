7880bfec4ac7af4de71037b7523e33b0ed69003a
Your Name <108307350+pjduri@users.noreply.github.com>
2024-05-21 14:30:06 -0500

    true false question class

---
+++ FunWithQuizzes/TrueFalse.cs
@@ -0,0 +1,23 @@
+﻿namespace FunWithQuizzes;
+
+public class TrueFalse : Question
+{
+    public bool CorrectAnswer { get; set; }
+
+    public TrueFalse(string question, int pointValue, bool correctAnswer) : base(question, pointValue)
+    {
+        CorrectAnswer = correctAnswer;
+    }
+
+    public override bool GetUserAnswer()
+    {
+        Console.WriteLine("Your answer (1 for true, 2 for false): ");
+        string? userAnswer = Console.ReadLine();
+        return userAnswer == "1";
+    }
+
+    public override double ScoreQuestion()
+    {
+        return GetUserAnswer() == CorrectAnswer ? PointValue : 0;
+    }
+}
